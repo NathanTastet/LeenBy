c43a3b5b6b48a6d8a43f05a492426a0db52d0706
Nathan Tastet <nathan.tastet69@gmail.Com>
2023-12-09 22:16:22 +0100

    te

--- js/main.js
+++ js/main.js
@@ -4,13 +4,43 @@
 
 // --- IMPORT ---
 
-import { initUI } from './ui.js';
 import { setup3D } from './vue3d.js';
+import { setupJoystick } from './joystick.js';
+import { setupMotorSliders} from './sliderControl.js'; 
+import { setupArmSelection, setup3DButton, setupControlButtons, setupPresetButtons, remiseazero} from './buttonControl.js';
 
-// --- FONCTION ---
+// --- FONCTIONS ---
 
 // Initialisation de l'application une fois que tout est chargé
 document.addEventListener("DOMContentLoaded", function() {
-  initUI();                // Initialise l'interface utilisateur
-  setup3D();               // Configure la vue 3d
+  setupJoystick();
+  setupMotorSliders();
+  setupControlButtons();
+  setupArmSelection();
+  setup3DButton();
+  setupPresetButtons();
+  setupLeftRightDivs();
+  setup3D();
+  remiseazero();
 });
+
+// Configure les interactions avec les divs 'left' et 'right'.
+function setupLeftRightDivs() {
+    const leftDiv = document.getElementById("left");
+    const rightDiv = document.getElementById("right");
+
+    rightDiv.addEventListener("click", function(){
+        leftDiv.style.backgroundColor = "#EAEDED";
+        leftDiv.style.zIndex = "1";
+        rightDiv.style.backgroundColor = "#F4F6F7";
+        rightDiv.style.zIndex = "2";
+    });
+
+
+    leftDiv.addEventListener("click", function() {
+        leftDiv.style.backgroundColor = "#F4F6F7";
+        leftDiv.style.zIndex = "2";
+        rightDiv.style.backgroundColor = "#EAEDED";
+        rightDiv.style.zIndex = "1";
+    });
+}
